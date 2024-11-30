809b1517de4b4dc7123dbc602ea58410b874631a
Francis Patron Fernandes <frpaf@eg.dk>
2024-11-30 19:15:39 +0530

    repair

--- android2/app/.cxx/Debug/491d5m2l/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/Users/francisfernandes/.gradle/caches/8.10.2/transforms/e15c1b005e6a52d37e7697ecaf0c7a1a/transformed/hermes-android-0.76.3-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/francisfernandes/.gradle/caches/8.10.2/transforms/e15c1b005e6a52d37e7697ecaf0c7a1a/transformed/hermes-android-0.76.3-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android2/app/.cxx/RelWithDebInfo/1d5d6517/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/Users/francisfernandes/.gradle/caches/8.10.2/transforms/9573087bf05abfa683888d8d081bbcd0/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/francisfernandes/.gradle/caches/8.10.2/transforms/9573087bf05abfa683888d8d081bbcd0/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
